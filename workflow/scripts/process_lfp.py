d33ab571a5135dc9d87d8c71bbbad5e5cb96c1fb
Sean Martin <seankieran.m@hotmail.com>
2022-06-10 17:42:20 +0100

    Add speed info to processing

--- workflow/scripts/process_lfp.py
+++ workflow/scripts/process_lfp.py
@@ -1,13 +1,15 @@
 """Process openfield LFP into power spectra etc. saved to NWB"""
 import itertools
 import logging
+from math import ceil, floor
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
+import scipy
 import simuran as smr
 from hdmf.common import DynamicTable
 from pynwb import TimeSeries
-from scipy.signal import coherence
 from simuran.loaders.nwb_loader import NWBLoader
 from skm_pyutils.table import df_from_file, df_to_file, list_to_df
 
@@ -35,6 +37,79 @@ def describe_coherence_columns():
         {"name": "frequency", "type": np.ndarray, "doc": "frequency values in Hz"},
         {"name": "coherence", "type": np.ndarray, "doc": "coherence values unitless"},
     ]
+
+
+def describe_speed_columns():
+    return [
+        {"name": "region", "type": str, "doc": "The brain region of the lfp"},
+        {"name": "speed", "type": np.ndarray, "doc": "The binned average speed"},
+        {
+            "name": "power",
+            "type": np.ndarray,
+            "doc": "The average lfp power in the same bins",
+        },
+    ]
+
+
+def bin_speeds_and_lfp(
+    speed, lfp_signal, samples_per_second, speed_sr, lfp_sr, low_f=None, high_f=None
+):
+    time_to_use = get_sample_times(len(speed), samples_per_second, speed_sr)
+    avg_speed = np.zeros_like(time_to_use, dtype=np.float32)
+    lfp_amplitudes = np.zeros_like(time_to_use, dtype=np.float32)
+    diff = 1 / (2 * samples_per_second)
+
+    for i, t in enumerate(time_to_use):
+        low_sample = floor((t - diff) * speed_sr)
+        high_sample = ceil((t + diff) * speed_sr)
+
+        avg_speed[i] = np.mean(speed[low_sample:high_sample])
+
+        low_sample = floor((t - diff) * lfp_sr)
+        high_sample = ceil((t + diff) * lfp_sr)
+        if high_sample >= len(lfp_signal):
+            module_logger.warning(
+                f"Position data ({time_to_use[-1]}s) is longer than EEG data ({len(lfp_signal) / lfp_sr}s)"
+            )
+            avg_speed = avg_speed[: len(lfp_signal)]
+            lfp_amplitudes = lfp_amplitudes[: len(lfp_signal)]
+            break
+        lfp_sample_200ms = lfp_signal[low_sample:high_sample]
+        abs_power, total_power = power_of_signal(
+            lfp_sample_200ms, lfp_sr, low_f, high_f
+        )
+        lfp_amplitudes[i] = abs_power / total_power
+
+    pd_df = list_to_df(
+        [avg_speed, lfp_amplitudes], transpose=True, headers=["Speed_", "power"]
+    )
+    pd_df["speed"] = np.around(pd_df["Speed_"])
+    pd_df.drop("Speed_", axis=1, inplace=True)
+
+    return pd_df
+
+
+def get_sample_times(n_samples, samples_per_second, sr):
+    skip_rate = int(sr / samples_per_second)
+    slicer = slice(skip_rate, -skip_rate, skip_rate)
+    return [i / sr for i in range(n_samples)][slicer]
+
+
+def power_of_signal(lfp_signal, lfp_sr, low_f, high_f):
+    slep_win = scipy.signal.hann(lfp_signal.size, False)
+    f, psd = scipy.signal.welch(
+        lfp_signal,
+        fs=lfp_sr,
+        window=slep_win,
+        nperseg=len(lfp_signal),
+        nfft=256,
+        noverlap=0,
+    )
+    idx_band = np.logical_and(f >= low_f, f <= high_f)
+    abs_power = scipy.integrate.simps(psd[idx_band], x=f[idx_band])
+    total_power = scipy.integrate.simps(psd, x=f)
+
+    return abs_power, total_power
 
 
 def process_lfp(ss, config):
@@ -65,6 +140,15 @@ def add_lfp_info(recording, config):
     did_anything.append(calculate_and_store_lfp_power(config, nwb_proc))
     did_anything.append(
         store_coherence(nwb_proc, flims=(config["fmin"], config["fmax"]))
+    )
+    did_anything.append(
+        store_speed_theta(
+            nwb_proc,
+            config["speed_theta_samples_per_second"],
+            config["theta_min"],
+            config["theta_max"],
+            recording.source_file,
+        )
     )
 
     for d in did_anything:
@@ -172,7 +256,7 @@ def store_coherence(nwb_proc, flims=None):
         x = average_signals[fd[0]].data[:]
         y = average_signals[fd[1]].data[:]
         fs = average_signals[fd[0]].rate
-        f, Cxy = coherence(x, y, fs, nperseg=2 * fs)
+        f, Cxy = scipy.signal.coherence(x, y, fs, nperseg=2 * fs)
 
         if flims is not None:
             fmin, fmax = flims
@@ -188,6 +272,39 @@ def store_coherence(nwb_proc, flims=None):
         df=results_df, name="coherence_table", columns=describe_coherence_columns()
     )
     mod = nwb_proc.create_processing_module("lfp_coherence", "Store coherence")
+    mod.add(hdmf_table)
+
+
+def store_speed_theta(nwbfile, samples_per_second, low_f, high_f, sf):
+    if "speed_theta" in nwbfile.processing:
+        return False
+    electrodes = nwbfile.electrodes.to_dataframe()
+    brain_regions = sorted(list(set(electrodes["location"])))
+    speed = nwbfile.processing["behavior"]["running_speed"].data[:]
+    speed_diff = np.mean(
+        np.diff(nwbfile.processing["behavior"]["running_speed"].timestamps)
+    )
+    if speed_diff == 0:
+        module_logger.error(f"No running speed detected in {sf}")
+    speed_sr = int(1.0 / speed_diff)
+    dfs = []
+    for region in brain_regions:
+        lfp_signal = nwbfile.processing["average_lfp"][f"{region}_avg"].data[:]
+        lfp_sr = nwbfile.processing["average_lfp"][f"{region}_avg"].rate
+
+        df = bin_speeds_and_lfp(
+            speed, lfp_signal, samples_per_second, speed_sr, lfp_sr, low_f, high_f
+        )
+        df.loc[:, "region"] = region
+        dfs.append(df)
+    final_df = pd.concat(dfs, ignore_index=True)
+
+    hdmf_table = DynamicTable.from_dataframe(
+        df=final_df, name="speed_lfp_table", columns=describe_speed_columns()
+    )
+    mod = nwbfile.create_processing_module(
+        "speed_theta", "Store speed theta relationship"
+    )
     mod.add(hdmf_table)
 
 

bb9265c9c773bfe5274dd1da946f5a24a3140b04
Sean Martin <seankieran.m@hotmail.com>
2022-10-12 18:27:12 +0100

    Improve single file

--- workflow/notebooks/convert_single_file.py
+++ workflow/notebooks/convert_single_file.py
@@ -1,9 +1,11 @@
 # %%
 %load_ext autoreload
 %autoreload 2
+import logging
 import sys
 from pathlib import Path
 
+import pynwb
 import simuran as smr
 from skm_pyutils.table import df_from_file, filter_table
 
@@ -12,10 +14,14 @@ sys.path.insert(0, str(parent / "scripts"))
 
 from convert_to_nwb import main
 
+module_logger = logging.getLogger("simuran.custom.convert_to_nwb")
+module_logger.setLevel(logging.DEBUG)
+module_logger.addHandler(logging.StreamHandler(sys.stdout))
+
 # %%
 table_path = r"E:\Repos\atn-sub-lfp-workflow\results\subret_recordings.csv"
 config_path = r"E:\Repos\atn-sub-lfp-workflow\config\simuran_params.yml"
-filename = "04092017_LSubRet1_+maze_trial_1_3.set"
+filename = "04092017_CSubRet1_+maze_trial_1_1.set"
 output_dir = Path("test")
 out_name = "test.csv"
 
@@ -32,6 +38,13 @@ rc = smr.RecordingContainer.from_table(filtered_table, loader=loader)
 r = rc[0]
 
 # %%
-main(table, config, filter_, output_dir, out_name, overwrite=True, debug=False)
+files = main(table, config, filter_, output_dir, out_name, overwrite=True, debug=False)
+
+# %%
+nwbfile = files[0]
+nwb_io = pynwb.NWBHDF5IO(nwbfile, "r")
+nwbfile = nwb_io.read()
+print(nwbfile.electrodes.to_dataframe())
+print(nwbfile.electrode_groups.keys())
 
 # %%

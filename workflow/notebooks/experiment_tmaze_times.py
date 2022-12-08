9600d8dcb3abff0a593b87f01cf70cb86a8fb31f
Sean Martin <seankieran.m@hotmail.com>
2022-12-08 10:32:46 +0000

    Add summary to tmaze

--- workflow/notebooks/experiment_tmaze_times.py
+++ workflow/notebooks/experiment_tmaze_times.py
@@ -3,7 +3,8 @@ import sys
 from pathlib import Path
 
 import simuran as smr
-from skm_pyutils.table import df_from_file
+from skm_pyutils.table import df_from_file, list_to_df, df_to_file
+import itertools
 
 parent = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(parent / "scripts"))
@@ -37,9 +38,26 @@ for t_dict, name in zip(ttimes, names):
     config["max_lfp_lengths"] = t_dict
     compute_and_save_coherence(out_dir, config, rc)
 
-# %%
+# # %%
 for name in names:
     res_loc = parent.parent / "results" / "tmaze_many" / name
     out_dir = parent.parent / "results" / "plots" / "tmaze_many" / name
     out_dir.mkdir(parents=True, exist_ok=True)
     main(res_loc, out_dir)
+
+# %%
+parts = ["start", "choice", "end"]
+trials = ["Forced", "Correct", "Incorrect"]
+groups = ["Control", "Lesion (ATNx)"]
+full_list = []
+for name in names:
+    df = df_from_file(parent.parent / "results" / "tmaze_many" / name / "results.csv")
+    for g, t, p in itertools.product(groups, trials, parts):
+        df_bit = (df["trial"] == t) & (df["part"] == p) & (df["Group"] == g)
+        theta_median = df[df_bit]["Theta Coherence"].median()
+        beta_median = df[df_bit]["Beta Coherence"].median()
+        full_list.append([name, f"{g}_{t}_{p}", theta_median, beta_median])
+
+headers = ["Times", "Grouping", "Theta Coherence", "Beta Coherence"]
+df = list_to_df(full_list, headers=headers)
+df_to_file(df, parent.parent / "results" / "tmaze_many" / "summary.csv")

4425b56650699f09e5328a47c61ef03245e7210a
Sean Martin <seankieran.m@hotmail.com>
2022-12-12 16:09:39 +0000

    Add t tmaze times test

--- workflow/notebooks/experiment_tmaze_times.py
+++ workflow/notebooks/experiment_tmaze_times.py
@@ -22,42 +22,40 @@ rc = smr.RecordingContainer.from_table(df, smr.loader("nwb"))
 # %%
 
 ttimes = [
-    {"choice": [1.0, 1.0], "start": 2.0, "end": 2.0},
-    {"choice": [1.5, 0.5], "start": 2.0, "end": 2.0},
-    {"choice": [3.5, 0.5], "start": 4.0, "end": 4.0},
-    {"choice": [2.0, 2.0], "start": 4.0, "end": 4.0},
-    {"choice": [1.5, 1.5], "start": 3.0, "end": 3.0},
+    {"choice": [0.9, 0.1], "start": 1.0, "end": 1.0},
+    {"choice": [0.8, 0.2], "start": 1.0, "end": 1.0},
+    {"choice": [1.3, 0.2], "start": 1.5, "end": 1.5},
 ]
 
-names = ["tmaze_2s", "tmaze_2s_uneven", "tmaze_4s", "tmaze_4s_mid", "tmaze_3s"]
-
-# %%
-for t_dict, name in zip(ttimes, names):
-    out_dir = parent.parent / "results" / "tmaze_many" / name
-    out_dir.mkdir(parents=True, exist_ok=True)
-    config["max_lfp_lengths"] = t_dict
-    compute_and_save_coherence(out_dir, config, rc)
-
-# # %%
-for name in names:
-    res_loc = parent.parent / "results" / "tmaze_many" / name
-    out_dir = parent.parent / "results" / "plots" / "tmaze_many" / name
-    out_dir.mkdir(parents=True, exist_ok=True)
-    main(res_loc, out_dir)
+names = ["tmaze_1s", "tmaze_1s_diff", "tmaze_1-5s"]
 
 # %%
 parts = ["start", "choice", "end"]
 trials = ["Forced", "Correct", "Incorrect"]
 groups = ["Control", "Lesion (ATNx)"]
+
+lfp_name = "tmaze_many_eeg"
+tmaze_val = True
 full_list = []
-for name in names:
-    df = df_from_file(parent.parent / "results" / "tmaze_many" / name / "results.csv")
+config["tmaze_egf"] = tmaze_val
+for t_dict, name in zip(ttimes, names):
+    out_dir = parent.parent / "results" / lfp_name / name
+    out_dir.mkdir(parents=True, exist_ok=True)
+    config["max_lfp_lengths"] = t_dict
+    compute_and_save_coherence(out_dir, config, rc)
+
+    res_loc = parent.parent / "results" / lfp_name / name
+    out_dir = parent.parent / "results" / "plots" / lfp_name / name
+    out_dir.mkdir(parents=True, exist_ok=True)
+    main(res_loc, out_dir)
+
+    df = df_from_file(parent.parent / "results" / lfp_name / name / "results.csv")
     for g, t, p in itertools.product(groups, trials, parts):
         df_bit = (df["trial"] == t) & (df["part"] == p) & (df["Group"] == g)
         theta_median = df[df_bit]["Theta Coherence"].median()
-        beta_median = df[df_bit]["Beta Coherence"].median()
+        beta_median = df[df_bit]["Theta Coherence"].mean()
         full_list.append([name, f"{g}_{t}_{p}", theta_median, beta_median])
 
-headers = ["Times", "Grouping", "Theta Coherence", "Beta Coherence"]
+headers = ["Times", "Grouping", "Theta Coherence", "Theta mean Coherence"]
 df = list_to_df(full_list, headers=headers)
-df_to_file(df, parent.parent / "results" / "tmaze_many" / "summary.csv")
+df_to_file(df, parent.parent / "results" / lfp_name / "summary.csv")

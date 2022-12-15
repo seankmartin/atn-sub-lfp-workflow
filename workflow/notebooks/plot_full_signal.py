20f504624317f104c0ec0b07cd3a8cda990b46a8
Sean Martin <seankieran.m@hotmail.com>
2022-12-15 20:32:33 +0000

    Add example plot

---
+++ workflow/notebooks/plot_full_signal.py
@@ -0,0 +1,19 @@
+# %%
+import sys
+from pathlib import Path
+
+import simuran as smr
+
+parent = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(parent / "scripts"))
+from plot_signals import plot_all_signals
+
+# %%
+loader = smr.loader("nwb")
+path_to_file = r"D:\atn-sub-lfp-workflow\results\processed\LRS1--t_maze--23032018_t2--S3--23032018_LRS1_+maze_t2_3.nwb"
+recording = smr.Recording(loader=loader, source_file=path_to_file)
+recording.load()
+nwbfile = recording.data
+output_path = Path("test_signals.png")
+
+plot_all_signals(recording, output_path)
